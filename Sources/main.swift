56b22c7978d130c4f09b8f53ad71bd140313bdba
Riccardo Crippa <therickys93@gmail.com>
2016-11-05 15:32:20 +0100

    almost ready

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,10 @@
 import Vapor
+import HTTP
 
 let drop = Droplet()
 
 drop.get("/") { _ in
-  return "It Just Works Team"
+    return Response(redirect: "/api")
 }
 
 drop.get("/api/", String.self) { request, countryCode in
@@ -13,7 +14,15 @@ drop.get("/api/", String.self) { request, countryCode in
 
 drop.get("/api/", String.self, String.self) { request, latitude, longitude in
     let response = try drop.client.get("http://scatter-otl.rhcloud.com/location?lat=\(latitude)&long=\(longitude)")
-    return response
+    if let countrycode = response.data["countrycode"]?.string {
+        let response = try drop.client.get("https://emergency-phone-numbers.herokuapp.com/country/\(countrycode)")
+        return response
+    }
+    return "not found"
+}
+
+drop.get("/api") { _ in
+    return "TODO: fare pagina per spiegare api"
 }
 
 drop.run()
