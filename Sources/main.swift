b62848eb10f6296037f2e387f28d80c03bdb2596
Riccardo Crippa <therickys93@gmail.com>
2016-11-08 16:28:01 +0100

    add the search of the phone numbers

--- Sources/main.swift
+++ Sources/main.swift
@@ -8,15 +8,13 @@ drop.get("/") { _ in
 }
 
 drop.get("/api/", String.self) { request, countryCode in
-    let response = try drop.client.get("https://emergency-phone-numbers.herokuapp.com/country/\(countryCode)")
-    return response
+    return DataSource().getCountryWithID(countryCode: countryCode)
 }
 
 drop.get("/api/", String.self, String.self) { request, latitude, longitude in
     let response = try drop.client.get("http://scatter-otl.rhcloud.com/location?lat=\(latitude)&long=\(longitude)")
     if let countrycode = response.data["countrycode"]?.string {
-        let response = try drop.client.get("https://emergency-phone-numbers.herokuapp.com/country/\(countrycode)")
-        return response
+        return DataSource().getCountryWithID(countryCode: countrycode)
     }
     return "not found"
 }
