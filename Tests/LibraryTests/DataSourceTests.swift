7cb04a71bfa4335cbff5e41b0d6041b490ae4f34
Riccardo Crippa <therickys93@gmail.com>
2016-11-12 14:47:58 +0100

    add other tests

--- Tests/LibraryTests/DataSourceTests.swift
+++ Tests/LibraryTests/DataSourceTests.swift
@@ -6,10 +6,39 @@ class DataSourceTests: XCTestCase {
 	func testClass() {
 		let _ = DataSource()
 	}
+    
+    func testGetNumbersFromCountryCode() {
+        let response = DataSource().getCountryWithID(countryCode: "it")
+        XCTAssertEqual(response, "{\"name\":\"Italy\", \"code\":\"IT\", \"police\":\"113\", \"medical\":\"118\", \"fire\":\"115\"}")
+    }
+    
+    func testGetNumbersFromCountryCodeError() {
+        let response = DataSource().getCountryWithID(countryCode: "hj")
+        XCTAssertEqual(response, "{\"error\":\"No country found with the given 2-letter country code hj\"}")
+    }
+    
+    func testEqualResponseForCountryInUpperOrLowerCase() {
+        let uppercase = DataSource().getCountryWithID(countryCode: "IT")
+        let lowercase = DataSource().getCountryWithID(countryCode: "it")
+        XCTAssertEqual(uppercase, lowercase)
+    }
+    
+    func testGetAllResults() {
+        let response = DataSource().getAll()
+        XCTAssertEqual(response, getAllOutput())
+    }
+    
+    func getAllOutput() -> String {
+        return DataSource().getAll()
+    }
 
 	static var allTests: [(String, (DataSourceTests) -> () throws -> Void)] {
 		return [
 			("testClass", testClass),
+			("testGetNumbersFromCountryCode", testGetNumbersFromCountryCode),
+			("testGetNumbersFromCountryCodeError", testGetNumbersFromCountryCodeError),
+			("testEqualResponseForCountryInUpperOrLowerCase", testEqualResponseForCountryInUpperOrLowerCase),
+			("testGetAllResults", testGetAllResults)
 		]
 	}
 }
