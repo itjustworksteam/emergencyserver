101ad576d9c88d3007da0debd50ae99ff7ad46d8
Riccardo Crippa <therickys93@gmail.com>
2017-02-22 16:41:12 +0100

    new dependencies

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-        //.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/vapor/mysql.git", majorVersion: 1, minor: 0)
     ]
 )
