5bbbabc439d5f0edb08792e0006865e0fb841017
Riccardo Crippa <therickys93@gmail.com>
2017-03-26 15:02:16 +0200

    add leaf to api page

--- Sources/App/Controllers/UserInterfaceController.swift
+++ Sources/App/Controllers/UserInterfaceController.swift
@@ -14,9 +14,23 @@ final class UserInterfaceController {
         return Response(redirect: "/developers")
     }
     
+    private let DEFAULT_URL = "https://emergency-server.herokuapp.com"
+    
     // MARK: Developer Page
     func developers(request: Request) throws -> ResponseRepresentable {
-        return try drop.view.make("api.html")
+        let apiOnes = try [
+            ["endpoint":"GET /api/all", "description":"Return a JSON array with Country objects.", "example":"\(DEFAULT_URL)/api/all"].makeNode(),
+            ["endpoint":"GET /api/:country", "description":"Return a JSON object with a Country object.", "example":"\(DEFAULT_URL)/api/it"].makeNode(),
+            ["endpoint":"GET /api/:latitude/:longitude", "description":"Return a JSON object with a Country object plus a \"closestcity\" field that is the closest city.", "example":"\(DEFAULT_URL)/api/45.0/9.0"].makeNode()
+        ].makeNode()
+        
+        let apiTwos = try [
+            ["endpoint":"GET /api/v2/numbers/all", "description":"Return a JSON array with Country objects.", "example":"\(DEFAULT_URL)/api/v2/numbers/all"].makeNode(),
+            ["endpoint":"GET /api/v2/numbers/:country", "description":"Return a JSON object with a Country object.", "example":"\(DEFAULT_URL)/api/v2/numbers/it"].makeNode(),
+            ["endpoint":"GET /api/v2/numbers/:latitude/:longitude", "description":"Return a JSON object with a Country object plus a \"closestcity\" field that is the closest city.", "example":"\(DEFAULT_URL)/api/v2/numbers/45.0/9.0"].makeNode()
+            ].makeNode()
+        
+        return try drop.view.make("api", Node(["apiOnes":apiOnes, "apiTwos":apiTwos]))
     }
     
 }
