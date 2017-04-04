590a48e45b35efc82f46ad07dd1e43930b9eed72
Riccardo Crippa <therickys93@gmail.com>
2017-04-04 15:56:59 +0200

    add some new feature

--- Tests/LibraryTests/DataSourceTests.swift
+++ Tests/LibraryTests/DataSourceTests.swift
@@ -14,7 +14,7 @@ class DataSourceTests: XCTestCase {
     
     func testGetNumbersFromCountryCodeError() {
         let response = DataSource().getCountryWithCountryCode("hj")
-        XCTAssertEqual(response, "{\"error\":\"No country found with the given 2-letter country code hj\"}")
+        XCTAssertEqual(response, errorCodeOutput("hj"))
     }
         
     func testEqualResponseForCountryInUpperOrLowerCase() {
@@ -38,6 +38,64 @@ class DataSourceTests: XCTestCase {
         XCTAssertEqual("Country={name=Italy, code=IT, police=112, medical=118, fire=115}", "\(country)")
     }
     
+    func testPoliceNumberWithCountryCode() {
+        let countryCode = "IT"
+        let response = DataSource().getPoliceNumberWithCountryCode(countryCode)
+        XCTAssertEqual(policeOutput(), response, "should be equals")
+        
+        let countryCode1 = "it"
+        let response1 = DataSource().getPoliceNumberWithCountryCode(countryCode1)
+        XCTAssertEqual(policeOutput(), response1, "should be equals")
+        
+        let countryCode2 = "hj"
+        let response2 = DataSource().getPoliceNumberWithCountryCode(countryCode2)
+        XCTAssertEqual(errorCodeOutput(countryCode2), response2, "should be equals")
+    }
+    
+    func testMedicalNumberWithCountryCode() {
+        let countryCode = "IT"
+        let response = DataSource().getMedicalNumberWithCountryCode(countryCode)
+        XCTAssertEqual(medicalOutput(), response, "should be equals")
+        
+        let countryCode1 = "it"
+        let response1 = DataSource().getMedicalNumberWithCountryCode(countryCode1)
+        XCTAssertEqual(medicalOutput(), response1, "should be equals")
+        
+        let countryCode2 = "hj"
+        let response2 = DataSource().getMedicalNumberWithCountryCode(countryCode2)
+        XCTAssertEqual(errorCodeOutput(countryCode2), response2, "should be equals")
+    }
+    
+    func testFireNumberWithCountryCode() {
+        let countryCode = "IT"
+        let response = DataSource().getFireNumberWithCountryCode(countryCode)
+        XCTAssertEqual(fireOutput(), response, "should be equals")
+        
+        let countryCode1 = "it"
+        let response1 = DataSource().getFireNumberWithCountryCode(countryCode1)
+        XCTAssertEqual(fireOutput(), response1, "should be equals")
+        
+        let countryCode2 = "hj"
+        let response2 = DataSource().getFireNumberWithCountryCode(countryCode2)
+        XCTAssertEqual(errorCodeOutput(countryCode2), response2, "should be equals")
+    }
+    
+    func policeOutput() -> String {
+        return "{\"police\":\"113\"}"
+    }
+    
+    func medicalOutput() -> String {
+        return "{\"medical\":\"118\"}"
+    }
+    
+    func fireOutput() -> String {
+        return "{\"fire\":\"115\"}"
+    }
+    
+    func errorCodeOutput(_ code: String) -> String {
+        return "{\"error\":\"No country found with the given 2-letter country code \(code)\"}"
+    }
+    
     func getAllOutput() -> String {
         return DataSource().getAll()
     }
@@ -51,6 +109,9 @@ class DataSourceTests: XCTestCase {
 			("testGetAllResults", testGetAllResults),
 			("testClosestCity", testClosestCity),
 			("testCustomStringConvertible", testCustomStringConvertible),
+			("testPoliceNumberWithCountryCode", testPoliceNumberWithCountryCode),
+			("testMedicalNumberWithCountryCode", testMedicalNumberWithCountryCode),
+			("testFireNumberWithCountryCode", testFireNumberWithCountryCode),
 		]
 	}
 }
