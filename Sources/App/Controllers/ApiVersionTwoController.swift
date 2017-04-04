40780a0c9852793c2869feafecd3eaac72c45aa6
Riccardo Crippa <therickys93@gmail.com>
2017-04-04 10:43:54 +0200

    add logger

--- Sources/App/Controllers/ApiVersionTwoController.swift
+++ Sources/App/Controllers/ApiVersionTwoController.swift
@@ -4,8 +4,17 @@ import Library
 
 final class ApiVersionTwoController {
     
+    private var logger: LogProtocol?
+    
+    private func logMessage(_ message: String){
+        if self.logger != nil {
+            self.logger?.debug(message)
+        }
+    }
+    
     func addRoutes(drop: Droplet){
         // /api/v2
+        self.logger = drop.log
         let v2 = drop.grouped("api/v2")
         // /api/v2/numbers
         let numbers = v2.grouped("numbers")
@@ -17,20 +26,28 @@ final class ApiVersionTwoController {
     // MARK: Version Two Numbers
     // GET /api/v2/numbers/all
     func getAll(request: Request) throws -> ResponseRepresentable {
-        return DataSource().getAll()
+        let response = DataSource().getAll()
+        logMessage(response)
+        return response
     }
     
     // GET /api/v2/numbers/:country
     func getWithCountryCode(request: Request, country: String) throws -> ResponseRepresentable {
-        return DataSource().getCountryWithCountryCode(country)
+        let response = DataSource().getCountryWithCountryCode(country)
+        logMessage(response)
+        return response
     }
     
     // GET /api/v2/numbers/:latitude/:longitude
     func getWithLatitudeAndLongitude(request: Request, latitude: String, longitude: String) throws -> ResponseRepresentable {
         let response = try drop.client.get(Emergency.createUrlWithLatitude(latitude, andLongitude: longitude))
+        logMessage(response.description)
         if let countryCode = response.data["countrycode"]?.string, let city = response.data["city"]?.string {
-            return DataSource().getCountryWithCountryCode(countryCode, andClosestCity: city)
+            let reply = DataSource().getCountryWithCountryCode(countryCode, andClosestCity: city)
+            logMessage(reply)
+            return reply
         }
+        logMessage(Emergency.noLocationFoundError())
         return Emergency.noLocationFoundError()
     }
 }
